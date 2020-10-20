76ee82b01c366e68782f171e3af2ef9381530662
susant07 <susant.git@gmail.com>
2020-10-20 17:39:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GGEditor, { Flow ,EditableLabel } from 'gg-editor';
+
+import "./styles.css";
+const data = {
+  nodes: [
+    {
+      id: '0',
+      label: 'Node',
+      x: 50,
+      y: 50,
+    },
+    {
+      id: '1',
+      label: 'Node',
+      x: 50,
+      y: 200,
+    },
+  ],
+  edges: [
+    {
+      label: 'Label',
+      source: '0',
+      sourceAnchor: 1,
+      target: '1',
+      targetAnchor: 0,
+    },
+  ],
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GGEditor className="editor">
+      <Flow
+        className="editorBd"
+        data={data}
+        graphConfig={{
+          defaultNode: {
+            shape: "bizFlowNode"
+          },
+          defaultEdge: {
+            shape: "bizFlowEdge"
+          }
+        }}
+      />
+      <EditableLabel />
+    </GGEditor>
   );
 }
 
